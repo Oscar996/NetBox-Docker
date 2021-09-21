785e04e2d79b2ecb2d6b0bc28f46e3b59e709789
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2021-09-21 14:53:23 -0400

    Update and rename Instalar-netbox-docker-compose.sh to Install-Netbox-Docker-compose.sh

--- Install-Netbox-Docker-compose.sh
+++ Install-Netbox-Docker-compose.sh
@@ -14,19 +14,12 @@ sudo apt-get install -y \
    git -y \
 
 
-
-#Chave Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
- "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 #Instalar Docker
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 #Instalar Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
@@ -51,6 +44,7 @@ docker-compose up -d
 
 #Rotina de inicialização dos containers junto ao sistema
 docker update --restart always netbox-docker_netbox_1 netbox-docker_postgres_1 netbox-docker_redis-cache_1 netbox-docker_netbox-worker_1 netbox-docker_redis_1
+                           
 
 #Alteração de parametros de configuracao
 #sed -e "s/^LOGIN_REQUIRED = environ.get('LOGIN_REQUIRED', 'False').lower() == 'true' .*/LOGIN_REQUIRED = True/g" \
@@ -69,5 +63,3 @@ echo Acessar o IP da máquina com a porta 8000
 echo Username: admin
 echo Password: admin
 echo Enjoy!!!
-
-
